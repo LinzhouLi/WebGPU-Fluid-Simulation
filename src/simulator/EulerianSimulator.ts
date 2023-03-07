8d1532110d163930077a8c7b0e4653f4bb0d2622
Your NameLinzhouLi <LinzhouLi@outlook.com>
2023-03-07 16:21:32 +0800

    release

--- src/simulator/EulerianSimulator.ts
+++ src/simulator/EulerianSimulator.ts
@@ -1,40 +1,7 @@
-import type { ResourceType } from '../common/resourceFactory';
-import { ResourceFactory } from '../common/resourceFactory';
-import { device } from '../controller';
-import { resourceFactory, bindGroupFactory } from '../common/base';
+
 
 class EulerianSimulator {
 
-  private static ResourceFormats = {
-  };
-
-  private timeStep: number;
-  private gridSize: { x: number; y: number; z: number };
-
-  private resource: Record<string, GPUBuffer | GPUTexture | GPUSampler>;
-
-  constructor(gridX: number, gridY: number, gridZ: number) {
-
-    this.gridSize = { x: gridX, y: gridY, z: gridZ };
-    this.timeStep = 0.04; // 25 fps
-    
-  }
-
-  public async initResource() {
-
-    this.resource = await resourceFactory.createResource(
-      [ 
-        'velocityCur', 'velocityNxt', 
-        'densityCur',  'densityNxt',
-        'pressureCur', 'pressureNxt',
-      ],
-      {
-        
-      }
-    );
-    
-
-  }
 
 }
 
