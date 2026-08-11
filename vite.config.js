6365eb9c821c6409dbba56c8e27ec90aba348469
Linzhou Li <LinzhouLi@outlook.com>
2026-08-11 15:21:16 +0800

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/WebGPU-Fluid-Simulation/',
+  base: './',
   build: {
     target: 'esnext',
-    outDir: 'docs'
+    outDir: 'build'
   }
 }));
